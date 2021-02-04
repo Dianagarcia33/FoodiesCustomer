1e5faab8ba619de42876d48e28076aac5b63a81e
Dianagarcia33 <41555299+Dianagarcia33@users.noreply.github.com>
2021-02-04 15:17:18 -0500

    commit 2:35 04/02/2021

--- app/src/main/java/com/foodies/customer/ActivitiesAndFragments/FragmentAdondeloLlevamos.java
+++ app/src/main/java/com/foodies/customer/ActivitiesAndFragments/FragmentAdondeloLlevamos.java
@@ -3,10 +3,12 @@ package com.foodies.customer.ActivitiesAndFragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.foodies.customer.R;
 
@@ -25,6 +27,9 @@ public class FragmentAdondeloLlevamos extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    Button buttonSiguiente;
+    Fragment fragmentInicioPrincipal,fragmentAdondeloLlevamos,fragmentQvacomprar;
+    FragmentTransaction fragmentTransaction;
 
     public FragmentAdondeloLlevamos() {
         // Required empty public constructor
@@ -61,6 +66,22 @@ public class FragmentAdondeloLlevamos extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragmen_adondelo_llevamos, container, false);
+        View view =  inflater.inflate(R.layout.fragment_fragmen_adondelo_llevamos, container, false);
+
+        buttonSiguiente = view.findViewById(R.id.btnAdondeLollevamos);
+
+
+        buttonSiguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentQvacomprar = new FragmentMquevacomprar();
+                fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.contenedorFragment,fragmentQvacomprar).commit();
+                fragmentTransaction.addToBackStack(null);
+            }
+        });
+
+        return view;
+
     }
 }
