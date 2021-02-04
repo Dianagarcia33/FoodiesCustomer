20a1b1b3927a734db706782523bc74b5f3564037
Dianagarcia33 <41555299+Dianagarcia33@users.noreply.github.com>
2021-02-04 18:16:19 -0500

    commit 6:16 04/02/2021

--- app/src/main/java/com/foodies/customer/ActivitiesAndFragments/FragmentPaquetes.java
+++ app/src/main/java/com/foodies/customer/ActivitiesAndFragments/FragmentPaquetes.java
@@ -3,10 +3,12 @@ package com.foodies.customer.ActivitiesAndFragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.foodies.customer.R;
 
@@ -22,9 +24,13 @@ public class FragmentPaquetes extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    Fragment fragmentInicioPrincipal,fragmentAdondeloLlevamos,fragmentQvacomprar,fragmentCart;
+    FragmentTransaction fragmentTransaction;
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    Button btnPaquetes;
 
     public FragmentPaquetes() {
         // Required empty public constructor
@@ -61,6 +67,20 @@ public class FragmentPaquetes extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_paquetes, container, false);
+        View view = inflater.inflate(R.layout.fragment_paquetes, container, false);
+
+        btnPaquetes = view.findViewById(R.id.btnPaquetes);
+
+        btnPaquetes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentCart = new CartFragment();
+                fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.contenedorFragment,fragmentCart).commit();
+                fragmentTransaction.addToBackStack(null);
+            }
+        });
+
+        return view;
     }
 }
